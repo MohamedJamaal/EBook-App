265abd4a53dbf9eb91622ab0d8b1f19c97611a22
mohamedjamaal <mohamed_gamal19@hotmail.com>
2024-11-19 17:28:03 +0200

    initalize app & structure folders & install get package

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:ebookapp/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const EbookApp());
+}
+
+class EbookApp extends StatelessWidget {
+  const EbookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: Splashview(),
+    );
+  }
+}
