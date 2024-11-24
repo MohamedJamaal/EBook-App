a696ddbd4872b6c544e6efecac02feb9e8a3266f
mohamedjamaal <mohamed_gamal19@hotmail.com>
2024-11-24 12:05:55 +0200

    add google fonts and custom text style file

--- lib/features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -1,8 +1,8 @@
 import 'package:ebookapp/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +13,7 @@ class CustomListViewItem extends StatelessWidget {
           borderRadius: BorderRadius.circular(16),
           image: const DecorationImage(
             image: AssetImage(AssetsData.testImage),
+            fit: BoxFit.cover,
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ebookapp/constants.dart';
 import 'package:ebookapp/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const EbookApp());
@@ -15,6 +16,7 @@ class EbookApp extends StatelessWidget {
     return GetMaterialApp(
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: KPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
       debugShowCheckedModeBanner: false,
       home: const Splashview(),
