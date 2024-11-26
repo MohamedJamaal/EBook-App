fff2250dca99ca381fc904ad413c601c45145155
mohamedjamaal <mohamed_gamal19@hotmail.com>
2024-11-26 09:49:10 +0200

    add styles file

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const KPrimaryColor = Color(0xff100B20);
 const KTransitionDuration = Duration(microseconds: 250);
+const KGtSectraFine = 'GT Sectra Fine';
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:ebookapp/core/utils/assets.dart';
 import 'package:ebookapp/core/utils/styles.dart';
 import 'package:ebookapp/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:ebookapp/features/home/presentation/views/widgets/featured_list_view.dart';
@@ -20,8 +21,39 @@ class HomeViewBody extends StatelessWidget {
           ),
           Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
           ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.testImage),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [],
+          )
         ],
       ),
     );
