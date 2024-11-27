6a52aedf8b1388a3123794c3822cd0b4854bd1cf
mohamedjamaal <mohamed_gamal19@hotmail.com>
2024-11-27 11:40:48 +0200

    add book details body & add its app bar

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,12 @@
+import 'package:ebookapp/features/home/presentation/views/book_details_view.dart';
 import 'package:ebookapp/features/home/presentation/views/home_view.dart';
 import 'package:ebookapp/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const KHomeView = '/homeView';
+  static const KBookDetailsView = '/bookDetailsView';
+
   static final router = GoRouter(routes: [
     GoRoute(
       path: '/',
@@ -12,6 +15,10 @@ abstract class AppRouter {
     GoRoute(
       path: KHomeView,
       builder: (context, state) => const HomeView(),
+    ),
+    GoRoute(
+      path: KBookDetailsView,
+      builder: (context, state) => const BookDetailsView(),
     ),
   ]);
 }
