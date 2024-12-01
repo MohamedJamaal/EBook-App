308c835cf9181d6fb563025b7b9478201fba1829
mohamedjamaal <mohamed_gamal19@hotmail.com>
2024-12-01 12:03:55 +0200

    add search view page

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,11 +1,13 @@
 import 'package:ebookapp/features/home/presentation/views/book_details_view.dart';
 import 'package:ebookapp/features/home/presentation/views/home_view.dart';
+import 'package:ebookapp/features/search/presentation/views/search_views.dart';
 import 'package:ebookapp/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const KHomeView = '/homeView';
   static const KBookDetailsView = '/bookDetailsView';
+  static const KSearchView = '/searchView';
 
   static final router = GoRouter(routes: [
     GoRoute(
@@ -19,6 +21,10 @@ abstract class AppRouter {
     GoRoute(
       path: KBookDetailsView,
       builder: (context, state) => const BookDetailsView(),
+    ),
+    GoRoute(
+      path: KSearchView,
+      builder: (context, state) => const SearchView(),
     ),
   ]);
 }
---
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -0,0 +1,55 @@
+import 'package:ebookapp/core/utils/styles.dart';
+import 'package:ebookapp/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:ebookapp/features/search/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:flutter/material.dart';
+
+class SearchViewBody extends StatelessWidget {
+  const SearchViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 30,
+        vertical: 30,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomSearchTextField(),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Search Results',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Expanded(
+            child: SearchResultLisView(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchResultLisView extends StatelessWidget {
+  const SearchResultLisView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: BookListViewItem(),
+        );
+      },
+    );
+  }
+}
