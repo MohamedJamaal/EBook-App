64ad5b14125d26bc8775cc2e5e7f0e2db7e42020
mohamedjamaal <mohamed_gamal19@hotmail.com>
2024-12-02 10:43:56 +0200

    add home repo file and methods

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:ebookapp/core/errors/failure.dart';
+import 'package:ebookapp/features/home/data/models/book_model/book_model.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+}
