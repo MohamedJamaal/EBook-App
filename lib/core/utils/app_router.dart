fecef41b64f350db41efef14715dda22000a8cc2
mohamedjamaal <mohamed_gamal19@hotmail.com>
2024-12-15 11:32:58 +0200

    fetch similar books cubit and states

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,7 +1,12 @@
+import 'package:ebookapp/core/utils/service_locator.dart';
+import 'package:ebookapp/features/home/data/models/book_model/book_model.dart';
+import 'package:ebookapp/features/home/data/repos/home_repo_impl.dart';
+import 'package:ebookapp/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:ebookapp/features/home/presentation/views/book_details_view.dart';
 import 'package:ebookapp/features/home/presentation/views/home_view.dart';
 import 'package:ebookapp/features/search/presentation/views/search_views.dart';
 import 'package:ebookapp/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
@@ -19,12 +24,17 @@ abstract class AppRouter {
       builder: (context, state) => const HomeView(),
     ),
     GoRoute(
-      path: KBookDetailsView,
-      builder: (context, state) => const BookDetailsView(),
-    ),
-    GoRoute(
       path: KSearchView,
       builder: (context, state) => const SearchView(),
+    ),
+    GoRoute(
+      path: KBookDetailsView,
+      builder: (context, state) => BlocProvider(
+        create: (context) => SimilarBooksCubit(
+          getit.get<HomeRepoImpl>(),
+        ),
+        child: BookDetailsView(bookModel: state.extra as BookModel),
+      ),
     ),
   ]);
 }
--- lib/features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,25 +1,39 @@
+import 'package:ebookapp/core/widgets/custom_error_widget.dart';
+import 'package:ebookapp/core/widgets/custom_loading_indicator.dart';
+import 'package:ebookapp/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:ebookapp/features/home/presentation/views/widgets/custom_Book_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SilmilarBooksListView extends StatelessWidget {
   const SilmilarBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.15,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 5),
-            child: CustomBookImage(
-              imageUrl:
-                  'https://images.adsttc.com/media/images/63d3/df58/7643/4a4d/2f1e/e451/medium_jpg/milan-architecture-city-guide-15-must-see-landmarks-and-15-contemporary-attractions-in-italys-fashion-capital_56.jpg?1674829706',
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+      builder: (context, state) {
+        if (state is SimilarBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.15,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  child: CustomBookImage(
+                    imageUrl:
+                        'https://images.adsttc.com/media/images/63d3/df58/7643/4a4d/2f1e/e451/medium_jpg/milan-architecture-city-guide-15-must-see-landmarks-and-15-contemporary-attractions-in-italys-fashion-capital_56.jpg?1674829706',
+                  ),
+                );
+              },
             ),
           );
-        },
-      ),
+        } else if (state is SimilarBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errorMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
